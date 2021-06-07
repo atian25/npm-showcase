b4e75852eeefd7fbde201ea98aceb6fc1243bc7a
TZ <atian25@qq.com>
2021-06-07 14:22:08 +0800

    feat: add unittest

--- index.js
+++ index.js
@@ -8,7 +8,11 @@ app.use(async ctx => {
   ctx.body = `hi, ${name}`;
 });
 
-app.listen(3000, () => {
-  console.log('server started at http://localhost:3000');
-});
+module.exports = app;
 
+// 当直接执行该文件时，启动服务。
+if (require.main === module) {
+  app.listen(3000, () => {
+    console.log('server started at http://localhost:3000');
+  });
+}
--- test/index.test.js
+++ test/index.test.js
@@ -1,6 +1,19 @@
 'use strict';
 
-describe('test/index.test.js', () => {
+const request = require('supertest');
+const app = require('..');
 
-  it('should work', () => {});
+describe('test/index.test.js', () => {
+  it('should work', () => {
+    return request(app.callback())
+      .get('/')
+      .expect(200, 'hi, anonymous');
+  });
+
+  it('should work with name', () => {
+    return request(app.callback())
+      .get('/')
+      .query('name=tz')
+      .expect(200, 'hi, tz');
+  });
 });
