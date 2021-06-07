192d46f11b39868fa8559238521059a13346d184
TZ <atian25@qq.com>
2021-06-07 14:01:47 +0800

    feat: use koa

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 'use strict';
 
-module.exports = 'hi, developer';
+const Koa = require('koa');
+const app = new Koa();
+
+app.use(async ctx => {
+  const name = ctx.query.name || 'anonymous';
+  ctx.body = `hi, ${name}`;
+});
+
+app.listen(3000, () => {
+  console.log('server started at http://localhost:3000');
+});
+
